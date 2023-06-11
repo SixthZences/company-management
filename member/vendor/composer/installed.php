07521070d72bd33e81bd0edb020ee37f5681c168
Patipan Khumklam <patipansixthz@gmail.com>
2023-06-11 17:50:28 +0700

    docker

---
+++ member/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'patip/project-web',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'claviska/simpleimage' => array(
+            'pretty_version' => '2.5.6',
+            'version' => '2.5.6.0',
+            'reference' => '5be7b5c912b94b85b20e423b9b8bd4680ccaa53b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../claviska/simpleimage',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'patip/project-web' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
